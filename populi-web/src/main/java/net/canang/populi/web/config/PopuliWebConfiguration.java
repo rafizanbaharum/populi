d559f382d8921e0118de2bb12802bcc732c8a57a
Rafizan Uda Baharum <rafizan.baharum@gmail.com>
2013-12-17 11:06:24 +0800

    Working on populi for intel

--- populi-web/src/main/java/net/canang/populi/web/config/PopuliWebConfiguration.java
+++ populi-web/src/main/java/net/canang/populi/web/config/PopuliWebConfiguration.java
@@ -55,7 +55,7 @@ public class PopuliWebConfiguration extends WebMvcConfigurerAdapter {
         properties.put("hibernate.format_sql", "false");
         properties.put("javax.persistence.validation.mode", "none");
         properties.put("hibernate.search.default.directory_provider", "filesystem");
-        properties.put("hibernate.search.default.indexBase", "./indexes");
+        properties.put("hibernate.search.default.indexBase", "/var/log/tomcat6/lucene/indexes");
 
         //properties.put("hibernate.connection.pool_size", "1");
         //properties.put("hibernate.format_sql", "true");
